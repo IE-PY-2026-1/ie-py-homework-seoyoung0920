118d643d7529146d8bd084d4866c3daaededddd6
seoyoung0920 <tjdud0920@mju.ac.kr>
2026-04-24 15:59:26 +0000

    60251796-홍서영 과제 2차 완료

--- main.py
+++ main.py
@@ -2,34 +2,36 @@
 # 작 성 자 :
 players = []
 
-# 등록할 선수 수 입력
-count = int(input("등록할 선수 수를 입력하세요: "))
+team_name = input("팀 이름을 입력하세요: ")
+bonus_rate = 0.1
 
-# 선수 정보 입력
+count = int(input("등록할 선수 수를 입력하세요 (최소 3명): "))
+
 for i in range(count):
     print(f"\n{i+1}번째 선수 입력")
     
     name = input("선수 이름: ")
     at_bat = int(input("타수: "))
     hit = int(input("안타: "))
-    
-    # 타율 계산
+
     if at_bat == 0:
-        average = 0
+        average = 0.0
     else:
         average = hit / at_bat
-    
-    #선수 데이터 리스트에 저장
-    player = [name, at_bat, hit, average]
-    players.append(player)
 
-# 결과 출력
-print("\n=== 선수 기록 ===")
+    players.append([name, at_bat, hit, average])
+
+players.sort(key=lambda x: x[3], reverse=True)
+
+total_avg = 0
+
+print(f"\n {team_name} 선수 기록")
 
 for player in players:
     name, at_bat, hit, average = player
 
-    # 등급 판정 (높은 기준부터)
+    total_avg += average
+
     if average >= 0.300:
         grade = "S"
     elif average >= 0.250:
@@ -39,15 +41,17 @@ for player in players:
     elif average >= 0.150:
         grade = "C"
     else:
-        grade = "F"
-    
-    # 결과 출력
-    print(f"\n선수 이름: {name}")
-    print(f"타수: {at_bat}")
-    print(f"안타: {hit}")
+        grade ="F"
+
+    if average >= 0.300 and hit >= 20:
+        title = "우수 타자"
+    elif average < 150 or at_bat < 5:
+        title = "훈련 필요"
+    else:
+        title = "일반 선수"
+
+    print(f"\n이름: {name}")
+    print(f"타수: {at_bat}, 안타: {hit}")
     print(f"타율: {average:.3f}")
     print(f"등급: {grade}")
-    
-    # 우수 선수 칭호
-    if average >= 0.300:
-        print("👉 우수 타자!")
+    print(f"칭호: {title}")
